961a6a69e94d557da965d04544d81d9463d7b5b2
Jon Thysell <thysell@gmail.com>
2017-08-22 12:32:13 -0700

    Updated examples with DB9 pinout

--- examples/SegaControllerKeyboardReader/SegaControllerKeyboardReader.ino
+++ examples/SegaControllerKeyboardReader/SegaControllerKeyboardReader.ino
@@ -51,6 +51,14 @@ output outputMap[] = {
 
 const byte BUTTONS = 12;
 
+// Controller DB9 pins (looking face-on to the end of the plug):
+//
+// 5 4 3 2 1
+//  9 8 7 6
+//
+// Connect pin 5 to +5V and pin 8 to GND
+// Connect the remaining pins to digital I/O pins (see below)
+
 // Specify the Arduino pins that are connected to
 // DB9 Pin 7, DB9 Pin 1, DB9 Pin 2, DB9 Pin 3, DB9 Pin 4, DB9 Pin 6, DB9 Pin 9
 SegaController controller(8, 2, 3, 4, 5, 6, 7);
